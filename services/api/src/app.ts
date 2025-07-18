dec83f248cc5b3fc4df85eca1fc6b29fae4acb72
TriHuynh <huynhminhtri1109@gmail.com>
2025-07-18 23:58:28 +0700

    chore(app.ts): add morgan lib to help log when dev and will change to combine or remove when product

--- services/api/src/app.ts
+++ services/api/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 import { connectMongoDB } from "./configs/mongoDBConfig";
 import { initializeFirebaseAdmin } from "./configs/firebaseAdminConfig";
@@ -14,6 +15,9 @@ const app = express();
 // Middleware cơ bản
 app.use(cors());
 app.use(express.json());
+
+// Ghi log theo format 'dev' (dành cho môi trường dev)
+app.use(morgan("dev"));
 
 // Mount routes
 //Sẽ sử dụng site.route để quản lý tất cả các route của ứng dụng
