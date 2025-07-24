6d8fc2b54a6b1dd98485cdf3e6aa8ac6a99cb68f
Duc Khai <hokhai84@gmail.com>
2025-07-24 12:39:01 +0700

    refactor route-controller-service-repo schools

--- services/api/src/models/Teacher.ts
+++ services/api/src/models/Teacher.ts
@@ -1,6 +1,12 @@
 import { Schema } from "mongoose";
 import { BaseUser } from "./BaseUser";
 
+export interface ITeacher {
+  literacy: string;
+  subjects: string[];
+  schoolId: Schema.Types.ObjectId;
+}
+
 export const Teacher = BaseUser.discriminator(
   "Teacher",
   new Schema({
