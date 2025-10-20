e7aa1646197beab15f6f5f301fd34676a17eba17
Duc Khai <hokhai84@gmail.com>
2025-10-20 15:56:52 +0700

    feat: changing targetSDK 32 to 33

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = 32
+        targetSdk = 33
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
