841ff0796457fcd30e5bc1451f7c28a32eb79eda
TriHuynh <huynhminhtri1109@gmail.com>
2025-10-27 22:20:35 +0700

    refactor: change the default theme is light

--- apps/omnimereduapp/lib/core/theme/theme_cubit.dart
+++ apps/omnimereduapp/lib/core/theme/theme_cubit.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ThemeCubit extends Cubit<ThemeMode> {
-  ThemeCubit() : super(ThemeMode.system);
+  ThemeCubit() : super(ThemeMode.light);
 
   void toggleTheme() {
     if (state == ThemeMode.light) {
